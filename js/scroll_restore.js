5e5ccf1b2532fcb8e9add79bdc4a6480a49110fe
qzbx <48089644+qzbx@users.noreply.github.com>
2021-07-16 13:02:27 +0900

    fix: パネル開閉状態の記憶・スクロール位置のバグを fix

--- js/scroll_restore.js
+++ js/scroll_restore.js
@@ -1,21 +1,29 @@
 // リロード時に直前のスクロール量を復元する
-const elm = document.getElementById("md");
+const checkbox = document.getElementById("nav-check"); // サイドパネル開閉ボタン
 const y = sessionStorage.getItem("poznote-scroll"); // スクロール量
 const b = sessionStorage.getItem("poznote-bottom"); // 底まで行ってたかのフラグ
 const uri = sessionStorage.getItem("poznote-uri"); // ロード前の URI
+const nav = sessionStorage.getItem("poznote-nav"); // ロード前のサイドパネル開閉状態
+
+if (nav) { // サイドパネル open
+  checkbox.checked = true;
+}
 
 if (y && location.href === uri) { // スクロールされてた＆ロード前と同じページ
-  if (b) { // 一番下までスクロールされてた場合は追従する
-    const bottom = elm.scrollHeight;
-    elm.scroll(0, bottom)
-  } else {
-    elm.scroll(0, y);
-  }
+  setTimeout(_ => { // ブラウザの挙動を上書きするための遅延
+    if (b) { // 一番下までスクロールされてた場合は追従する
+      const bottom = document.body.scrollHeight;
+        window.scrollTo(0, bottom);
+    } else {
+        window.scrollTo(0, y);
+    }
+  }, "540"); // msec
 }
 
 // SS のお掃除
 sessionStorage.removeItem("poznote-scroll");
 sessionStorage.removeItem("poznote-bottom");
 sessionStorage.removeItem("poznote-uri");
+sessionStorage.removeItem("poznote-nav");
 
 
